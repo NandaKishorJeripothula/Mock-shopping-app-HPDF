25c32a9937a8868b6d4eb7a9050eed37122ea9f1
agzuniverse <aswinganesh666@gmail.com>
2018-02-08 21:54:02 +0530

    Search backend integration complete

--- components/Main.js
+++ components/Main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {StyleSheet, View, TouchableOpacity, ScrollView} from 'react-native';
+import {StyleSheet, View, TouchableOpacity, ScrollView, ToastAndroid} from 'react-native';
 import {Text, Icon, Form, Item, Input, Button, Container} from 'native-base';
 import Store from './Store';
 import ConImg from './ConImg';
@@ -12,7 +12,8 @@ class Main extends React.Component {
     constructor(props){
         super(props);
         this.state={
-            products:[<View style={{height:0,width:0}}/>]
+            products:[<View style={{height:0,width:0}}/>],
+            searchText:''
         };
     }
 
@@ -48,16 +49,74 @@ class Main extends React.Component {
         });
     }
 
+    c=0;
+
+    searchResults=() => {
+        ToastAndroid.show("Searching...",ToastAndroid.LONG);
+        this.setState({
+            products:[<View style={{height:0,width:0}}/>]
+        });
+        fetch('https://app.banner20.hasura-app.io/search',
+        {
+            method:'post',
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "data": {
+                    "search": this.state.searchText
+                }       
+            })
+        }).then((res) => {
+            return res.json();
+        }).then((data) => {
+            console.log(data);
+            if(data.length==0){
+                this.setState({
+                    products:<Text style={styles.noresults}>No Results Found</Text>
+                });
+            }
+            else{
+                for(i in data){
+                    this.setState({
+                        products:this.state.products.concat([
+                            <View style={styles.con}>
+                                <TouchableOpacity onPress={() => {this.props.navigation.navigate('Productpage',{product_id:data[i].id})}}>
+                                    <ConImg 
+                                    imageSource={{uri:data[i].first_image_url}}
+                                    header = {data[i].name}
+                                    />
+                                </TouchableOpacity>
+                            </View>                        
+                        ])
+                    });
+                    this.c++;
+                    if(this.c==2){
+                        this.c=0;
+                        this.setState({
+                            products:this.state.products.concat([<View style={styles.conBanner}/>])
+                        });
+                    }
+                }
+            }
+        });
+    }
+
     render() {
         return(
             <ScrollView>
                 <Form style={{flexDirection:'row'}}>
                     <Item rounded bordered style={styles.searchbar}>
-                        <Input placeholder="Search..."/>
+                        <Input placeholder="Search..." onChangeText={(text) => {this.setState({searchText:text})}} value={this.state.searchText}/>
                     </Item>
-                    <Button onPress={() => {this.props.navigation.navigate('Filters')}} light style={styles.filterButton}>
+                    <Button onPress={this.searchResults} light style={styles.filterButton}>
+                        <Icon name='search'/>
+                    </Button>
+                    {/*<Button onPress={() => {this.props.navigation.navigate('Filters')}} light style={styles.filterButton}>
                         <Icon name='settings'/>
                     </Button>
+                    */}
                 </Form>
                 <View style={styles.comContainer}>
                     {this.state.products}
@@ -82,7 +141,7 @@ const styles = StyleSheet.create({
         flex:1,
         flexDirection:'row',
         flexWrap:'wrap',
-        padding:5
+        padding:5,
     },
     con:{
      flex: 1,
@@ -93,6 +152,11 @@ const styles = StyleSheet.create({
         alignItems:'center',
         justifyContent :'center',
         padding:5
+    },
+    noresults:{
+        fontSize:30,
+        color:'rgba(0,0,0,0.4)',
+        marginLeft:70,
     }
 });
 
